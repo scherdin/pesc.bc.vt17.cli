0cde5c0f4b6645d1f9a69ecc442558ab2295fa2c
Petter Scherdin <scherdin@gmail.com>
2017-06-02 15:45:53 +0200

    added bootstrap 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AlertModule } from 'ngx-bootstrap';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     AlertModule.forRoot(),
+    BsDropdownModule.forRoot(),
     BrowserModule
   ],
   providers: [],
