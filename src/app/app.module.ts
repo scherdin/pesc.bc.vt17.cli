c26d40aa9416b018ff46d4db19143ea1737a83ed
Petter Scherdin <scherdin@gmail.com>
2017-06-02 13:25:34 +0200

    added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AlertModule } from 'ngx-bootstrap';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,6 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    AlertModule.forRoot(),
     BrowserModule
   ],
   providers: [],
