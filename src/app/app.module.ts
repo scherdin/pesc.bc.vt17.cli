021a6fc0be534b57e6b3abb07178951d99f23e0a
Petter Scherdin <scherdin@gmail.com>
2017-06-05 22:47:05 +0200

    stylish theme in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,20 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { FooterComponent } from './footer/footer.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavComponent
+    NavComponent,
+    AboutComponent,
+    ServicesComponent,
+    FooterComponent,
+    PortfolioComponent
   ],
   imports: [
     AlertModule.forRoot(),
